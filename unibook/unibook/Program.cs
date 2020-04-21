6e7175ce38908e2e6dd97d65609e713b96470974
Kasper <kmadse16@student.aau.dk>
2020-04-21 13:52:18 +0200

    Added database connect

--- unibook/unibook/Program.cs
+++ unibook/unibook/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace unibook
 {
@@ -14,8 +16,22 @@ namespace unibook
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            var d = new Test();
-            d.hejmor();
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "UNIBOOK";
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "SELECT * FROM Books;";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnOne = reader.GetString(1);
+                    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                }
+                dbCon.Close();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
