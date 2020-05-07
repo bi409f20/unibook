2d3884d81814d51eed6d01679bf3372aa06ec3b5
Kasper <kmadse16@student.aau.dk>
2020-05-07 21:44:42 +0200

    tak anders

--- unibook/unibook/Pages/Index.cshtml.cs
+++ unibook/unibook/Pages/Index.cshtml.cs
@@ -13,20 +13,29 @@ namespace unibook.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly UnibookContext context;
+        private readonly UnibookContext _context;
 
         public IndexModel(ILogger<IndexModel> logger, UnibookContext context)
         {
             _logger = logger;
-            this.context = context;
+            _context = context;
         }
 
         public void OnGet()
         {
-            Book = context.Books.FirstOrDefault();
+            Books = _context.Books.ToList(); // SELECT * FROM Books;
         }
 
-        public Book Book { get; set; }
+        public void OnPost(string query)
+        {
+            query = (query ?? "").ToLower();
+
+            Books = _context.Books
+                .Where(b => b.Title.ToLower().Contains(query) || b.ISBN.ToLower().Contains(query) || b.Author.ToLower().Contains(query))
+                .ToList(); // SELECT * FROM Books WHERE title LIKE '%test%';
+        }
+
+        public List<Book> Books { get; set; }
         public Listing listing { get; set; }
         public User user { get; set; }
 
