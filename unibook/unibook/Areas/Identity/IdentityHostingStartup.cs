0a9837a5a63d2ef57cd4d886ebabdae435f24ae0
Kasper <kmadse16@student.aau.dk>
2020-05-08 12:54:41 +0200

    authentication pages

---
+++ unibook/unibook/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using unibook.Data;
+using unibook.Models;
+
+[assembly: HostingStartup(typeof(unibook.Areas.Identity.IdentityHostingStartup))]
+namespace unibook.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
