964c0d1fe32795ff4a8227a73c1d24443f0fe1b2
Kasper <kmadse16@student.aau.dk>
2020-05-08 12:03:27 +0200

    Signupmodel og controller :fire:

--- unibook/unibook/Pages/Signup.cshtml.cs
+++ unibook/unibook/Pages/Signup.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,11 +8,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace unibook
 {
-    public class CreateListingModel : PageModel
+    public class SignupModel : PageModel
     {
         public void OnGet()
         {
 
         }
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password",
+            ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        
     }
+
 }
