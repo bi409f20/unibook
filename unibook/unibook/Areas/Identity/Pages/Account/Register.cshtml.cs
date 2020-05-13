0ff5ffb83f9c2632003de1da9db6140f8c0e8a03
Sherm24 <mherma18@student.aau.dk>
2020-05-13 16:56:25 +0100

    suck ma dick kasper madsen

--- unibook/unibook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ unibook/unibook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +17,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using unibook.Models;
+using unibook.Data;
 
 namespace unibook.Areas.Identity.Pages.Account
 {
@@ -23,21 +27,34 @@ namespace unibook.Areas.Identity.Pages.Account
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
         private readonly ILogger<RegisterModel> _logger;
+        private readonly IHostingEnvironment hostingEnvironment;
+        private readonly UnibookContext _context;
 
         public RegisterModel(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
-            ILogger<RegisterModel> logger)
+            ILogger<RegisterModel> logger,
+            IHostingEnvironment environment,
+            UnibookContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            this.hostingEnvironment = environment;
+            _context = context;
         }
 
         [BindProperty]
         public InputModel Input { get; set; }
 
-        public string ReturnUrl { get; set; }
+        [BindProperty]
+        public new User User { set; get; }
+
+        [BindProperty]
+        public IFormFile Image { set; get; }
+    
+
+    public string ReturnUrl { get; set; }
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
@@ -86,8 +103,16 @@ namespace unibook.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new User { Email = Input.Email, UserName = Input.Email, Name = Input.Name };
+
+                var fileName = GetUniqueName(Image.FileName);
+                var Images = Path.Combine(hostingEnvironment.WebRootPath, "Images");
+                var filePath = Path.Combine(Images, fileName);
+                this.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+                User.ImageName = fileName; // Set the file name
+
+                var user = new User { Email = Input.Email, UserName = Input.Email, Name = Input.Name, University = Input.University, ImageName = Input.ImageName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
+              
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
@@ -99,10 +124,18 @@ namespace unibook.Areas.Identity.Pages.Account
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
+
             }
 
             // If we got this far, something failed, redisplay form
             return Page();
+        }
+        private string GetUniqueName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return Path.GetFileNameWithoutExtension(fileName)
+                   + "_" + Guid.NewGuid().ToString().Substring(0, 4)
+                   + Path.GetExtension(fileName);
         }
     }
 }
