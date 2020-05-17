665cb94858fdd01ecd681b376aa2c03a60835c11
Jacob HC <jhch18@student.aau.dk>
2020-05-17 23:19:19 +0200

    Edit profile, but image no working :)

--- unibook/unibook/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ unibook/unibook/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using unibook.Models;
 
 namespace unibook.Areas.Identity.Pages.Account.Manage
@@ -14,13 +18,19 @@ namespace unibook.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IHostingEnvironment hostingEnvironment;
 
         public IndexModel(
             UserManager<User> userManager,
-            SignInManager<User> signInManager)
+            SignInManager<User> signInManager,
+            ILogger<RegisterModel> logger,
+            IHostingEnvironment environment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+             _logger = logger;
+            this.hostingEnvironment = environment;
         }
 
         public string Username { get; set; }
@@ -30,12 +40,29 @@ namespace unibook.Areas.Identity.Pages.Account.Manage
 
         [BindProperty]
         public InputModel Input { get; set; }
+        [BindProperty]
+        public User Profile { set; get; }
+        [BindProperty]
+        public IFormFile ImageNameInput { set; get; }
 
         public class InputModel
         {
+            [Required]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+            [Display(Name = "City")]
+            public string City { get; set; }
+            [Display(Name = "Postal code")]
+            public string PostalCode { get; set; }
+            [Display(Name = "Address")]
+            public string Address { get; set; }
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+            [Display(Name = "University")]
+            public string University { get; set; }
+            [Display(Name = "Image")]
+            public string ImageName { get; set; }
         }
 
         private async Task LoadAsync(User user)
@@ -47,7 +74,13 @@ namespace unibook.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                Name = user.Name,
+                City = user.City,
+                PostalCode = user.PostalCode,
+                Address = user.Address,
+                PhoneNumber = phoneNumber,
+                University = user.University,
+                ImageName= user.ImageName,
             };
         }
 
@@ -70,11 +103,19 @@ namespace unibook.Areas.Identity.Pages.Account.Manage
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-
             if (!ModelState.IsValid)
             {
                 await LoadAsync(user);
                 return Page();
+            }
+            if (ImageNameInput != null)
+            {
+                var fileName = GetUniqueName(ImageNameInput.FileName);
+                var Images = Path.Combine(hostingEnvironment.WebRootPath, "Images/UserImages");
+                var filePath = Path.Combine(Images, fileName);
+                this.ImageNameInput.CopyTo(new FileStream(filePath, FileMode.Create));
+                this.Profile.ImageName = fileName; // Set the file name
+                user.ImageName = fileName;
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
@@ -87,10 +128,39 @@ namespace unibook.Areas.Identity.Pages.Account.Manage
                     throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
                 }
             }
+            if (Input.Name != user.Name)
+            {
+                user.Name = Input.Name;
+            }
+            if (Input.City != user.City)
+            {
+                user.City = Input.City;
+            }
+            if (Input.PostalCode != user.PostalCode)
+            {
+                user.PostalCode = Input.PostalCode;
+            }
+            if (Input.Address != user.Address)
+            {
+                user.Address = Input.Address;
+            }
+            if (Input.University != user.University)
+            {
+                user.University = Input.University;
+            }
+
+            await _userManager.UpdateAsync(user);
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
+        }
+        private string GetUniqueName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return Path.GetFileNameWithoutExtension(fileName)
+                   + "_" + Guid.NewGuid().ToString().Substring(0, 4)
+                   + Path.GetExtension(fileName);
         }
     }
 }
