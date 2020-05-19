bda1b6d53a18c565f7999a32793cb3d83c21466f
Kasper <kmadse16@student.aau.dk>
2020-05-19 15:27:20 +0200

    Search results page

--- unibook/unibook/Pages/SearchResult.cshtml.cs
+++ unibook/unibook/Pages/SearchResult.cshtml.cs
@@ -5,38 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using unibook.Models;
 using unibook.Data;
+using unibook.Models;
 
 namespace unibook.Pages
 {
-    public class IndexModel : PageModel
+    public class SearchResultModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly UnibookContext _context;
-
-        public IndexModel(ILogger<IndexModel> logger, UnibookContext context)
+        public SearchResultModel(ILogger<IndexModel> logger, UnibookContext context)
         {
             _logger = logger;
             _context = context;
         }
 
-        public void OnGet()
-        {
-            Listings = _context.Listings.ToList(); // SELECT * FROM Listings;
-        }
-
-        public IActionResult OnPost(string SearchString)
+        public void OnGet(string SearchString)
         {
             SearchString = (SearchString ?? "").ToLower();
 
             Listings = _context.Listings
                 .Where(b => b.Book.Title.ToLower().Contains(SearchString) || b.Book.ISBN.ToLower().Contains(SearchString) || b.Book.Author.ToLower().Contains(SearchString))
                 .ToList(); // SELECT * FROM Listings WHERE title LIKE '%test%';
-            return RedirectToPage("./SearchResults");
         }
+        public void OnPost(string SearchString)
+        {
+            SearchString = (SearchString ?? "").ToLower();
 
-
+            Listings = _context.Listings
+                .Where(b => b.Book.Title.ToLower().Contains(SearchString) || b.Book.ISBN.ToLower().Contains(SearchString) || b.Book.Author.ToLower().Contains(SearchString))
+                .ToList(); // SELECT * FROM Listings WHERE title LIKE '%test%';
+        }
         public List<Book> Books { get; set; }
         public Listing listing { get; set; }
         public User user { get; set; }
